4047aafc16d3b6f02e8479c11c000b129f581608
Tony Wolski <tony@awolski.com>
2023-03-23 09:29:47 +0000

    Add 'apply' argument to dev.sh script

--- bin/dev.sh
+++ bin/dev.sh
@@ -7,12 +7,13 @@ if [[ ! -d "lib" ]]; then
     pip install pillow -t lib
 fi
 
-if [ $# -eq 1 && $1 == "apply" ]; then
+# If 'apply' provided, do terraform stuff
+if [[ $# -eq 1 && $1 == "apply" ]]; then
     terraform init
     terraform apply -auto-approve
+    exit 0
 fi
 
-if [ $# -eq 0 || $1 != "apply" ]; then
-    echo "No or invalid arguments supplied. Entering dev mode... "
-    tail -f /dev/null
-else
+# Othwerwise just leave the container running
+echo "No or invalid arguments supplied. Entering dev mode... "
+tail -f /dev/null
